8c5b56bcf7a1bdaba751f5cd7885772068fd1392
litetex <40789489+litetex@users.noreply.github.com>
2025-10-25 15:15:43 +0200

    Wait for zone file on initial clone

--- dns-client-infra/rbldnsd/_build/scripts/init.sh
+++ dns-client-infra/rbldnsd/_build/scripts/init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+until [ -f /zonefiles/asn.zone ]
+do
+    echo "Failed to find zone file! Waiting a moment"
+    sleep 15
+done
+
 rbldnsd -n -r /zonefiles -b ::0/5353 v4.asnresolver.internal:ip4trie:asn.zone v6.asnresolver.internal:ip6trie:asn6.zone
